3961a95dadb2dbe9bfd47d0a17ec23cf02bf53e2
Mauricio <mauriciobraga100@gmail.com>
2024-10-27 14:48:15 -0300

    Adicionando e testando projeto com venv

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("teste") 
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+
+reader = SimpleMFRC522()
+
+try:
+    print("Aproxime a tag RFID...")
+    id = reader.read_id()
+    print(f"ID da Tag: {id}")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
+finally:
+    GPIO.cleanup()
