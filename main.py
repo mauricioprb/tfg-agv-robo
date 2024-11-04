13bbec3466e002a5edc50b09c844462aa613a703
Mauricio Pereira Braga <mauriciobraga100@gmail.com>
2024-11-04 03:45:21 -0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from controle_motores import ControleMotores
-
-if __name__ == "__main__":
-    controle = ControleMotores(motor_direito_gpio=12, motor_esquerdo_gpio=13)
-    controle.executar_movimento()
