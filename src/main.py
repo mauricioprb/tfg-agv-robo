f0404d89181b8833ba49c3fa05e43960db193945
Mauricio Pereira Braga <mauriciobraga100@gmail.com>
2024-11-08 19:45:48 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,20 @@ from controle_motores import ControleMotores
 from sensor_monitor import SensorMonitor
 
 if __name__ == "__main__":
-    # controle = ControleMotores(motor_direito_gpio=12, motor_esquerdo_gpio=13)
-    # controle.executar_movimento()
+    controle = ControleMotores(motor_direito_gpio=12, motor_esquerdo_gpio=13)
     pinos_sensor = [4, 17, 27, 22, 5, 6, 13, 19]
     monitor = SensorMonitor(pinos_sensor, intervalo=1)
+    pista = [sign(i) for i in monitor.monitorar()]
 
-    monitor.monitorar()
+    index = pista.find(1)
+    quantidade = pista.count(1)
+    movimento = {
+        (3, 2):(2500, 2500)
+    }
+
+    try:
+        velocidades = movimento[(index, quantidade)]
+
+    except:
+        velocidades = (0, 0)
+    controle.executar_movimento(velocidades[0], velocidades[1])
